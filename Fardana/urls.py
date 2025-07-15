1d9115f8286f99d937d1303b8ddcae908d86c64d
Nazanin-Hosseini-afzal <hosseiniafzalnazaninzahra@gmail.com>
2025-07-14 21:58:52 -0700

    انتقال قالب به پروژه
    انقال فایل ها ی قالب

--- Fardana/urls.py
+++ Fardana/urls.py
@@ -15,14 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path , include
-from website.views import *
+from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),    
-    path('home/', index_view),    
-    path('about/', about_view),    
-    path('contact/', contact_view),    
+    path('admin/', admin.site.urls),  
+    path('', include('website.url')),  
 
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+
--- website/url.py
+++ website/url.py
@@ -1,9 +1,9 @@
-from django.urls import path , include
+from django.urls import path 
 from website.views import *
 
 
 urlpatterns = [
-    path('home/', index_view),    
+    path('', index_view),    
     path('about/', about_view),    
     path('contact/', contact_view),    
 
